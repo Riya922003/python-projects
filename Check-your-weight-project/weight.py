b0978eb3112887610b92b02e4446e9ea8bf03dc5
Riya Gupta <139098932+Riya922003@users.noreply.github.com>
2024-11-01 13:29:53 +0530

    Create weight.py

---
+++ Check-your-weight-project/weight.py
@@ -0,0 +1,12 @@
+
+
+weight = int(input("Enter your Weight : "))
+# print()
+unit =input("(L)bs or (K)g: ")
+
+if unit.lower()=='k':
+    ans = weight/0.453592
+else:
+    ans = weight*0.453592
+
+print(ans)
