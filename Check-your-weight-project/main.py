c1f3f79d8a2fa703a3de5f2141059a4c526f1215
Riya Gupta <139098932+Riya922003@users.noreply.github.com>
2024-11-01 19:52:57 +0530

    Update main.py

--- Check-your-weight-project/main.py
+++ Check-your-weight-project/main.py
@@ -1,12 +1,9 @@
+weight = int(input("Enter your weight: "))
+unit = input("(L)bs or (K)g: ")
 
-
-weight = int(input("Enter your Weight : "))
-# print()
-unit =input("(L)bs or (K)g: ")
-
-if unit.lower()=='k':
-    ans = weight/0.453592
+if unit.lower() == 'k':
+    ans = weight / 0.453592  # Convert kilograms to pounds
 else:
-    ans = weight*0.453592
+    ans = weight * 0.453592  # Convert pounds to kilograms
 
 print(ans)
